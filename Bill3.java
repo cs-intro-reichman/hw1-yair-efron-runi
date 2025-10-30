22fcdb2a588543fcc2c5a2dda92648c3bb5d8282
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-10-30 20:25:07 +0200

    done

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+			String name2=args[1];
+			String name3=args[2];
+			int sum=Integer.parseInt(args[3]);
+			double bill=sum;
+			bill=Math.ceil(bill/3);
+			System.err.println(name3+" "+name2+" "+name1+" Pay "+bill+" Shekels each.");
+
 			//new rhing
 	    // Replace this comment with the rest of your code   
 	}
